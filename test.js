0193f9cda4c4d6f2d9b87c2653ec0d676240b5a7
Mohsen farhadi <farhadi@gmail.com>
2020-01-03 00:01:10 +0330

    test  callback in shahkar service

--- test.js
+++ test.js
@@ -1,23 +1,30 @@
 const  IDChecker = require('./nationalcodechecker.js');
-
-idChecker = new IDChecker(null,'0068812991','1400508330','09121872491');
-
-idChecker.isVerified().then(res=>{
+global.SHAHKAR_USER='tech@kuknos.org';
+global.SHAHKAR_PASS='Ci699!';
+idChecker = new IDChecker(1,'0068812991','14005083301','09352492937');
+console.log('1===>');
+//async function ttt(){
+console.log('2===>');
+ idChecker.isVerified(res=>{
 /*console.log("--->",idChecker.personality,
 	    idChecker.corpID ,//idChecker.idChecker.nationalID,
 	    idChecker.mobileNumber,
 	    idChecker.nationalCode);*/
 
+console.log('3===>'+res);
 if (res){
 	console.log('verified. :)');
 }else{
 	console.log('Not verified. :(');
 }
-});
+//});
+//}
+//ttt().then(result =>{;
 
-
+console.log('4===>');
 console.log(idChecker.personality,
 	    idChecker.corpID,
 	    idChecker.mobileNumber,
 	    idChecker.nationalCode);
 console.log("What???!!!");
+});
